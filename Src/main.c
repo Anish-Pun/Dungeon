10ba9c96bdc53ec3c1d9dad7f3ce10e10e15d173
Anish <145456302+Anish-Pun@users.noreply.github.com>
2025-05-11 20:25:23 +0200

    Add initial implementation of dungeon and player modules

---
+++ Src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dungeon.h"
+
+int main()
+{
+    void WelcomeMsg(void);
+    
+    return 0;
+}
+
+
+void WelcomeMsg(void)
+{
+    printf("     __        __   _                          \n");
+    printf("    \\ \\      / /__| | ___ ___  _ __ ___   ___ \n");
+    printf("     \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n");
+    printf("      \\ V  V /  __/ | (_| (_) | | | | | |  __/\n");
+    printf("       \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|\n");
+}
