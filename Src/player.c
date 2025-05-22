0f75c063fcaab8dc73bd5b9b3b483d727f5585ba
Anish <145456302+Anish-Pun@users.noreply.github.com>
2025-05-22 16:03:07 +0200

    Removed Duped Movement Function

--- Src/player.c
+++ Src/player.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// #include "save_load.h"
+#include "save_load.h"
 
 // Initialize the player
 void InitializePlayer(struct Player *player)
@@ -38,55 +38,6 @@ void InitializePlayer(struct Player *player)
     printf("|   HP     = %d                            |\n", player->hp);
     printf("|   Damage = %d                            |\n", player->damage);
     printf("+------------------------------------------+\n");
-}
-
-// Move the player to a different room
-void MovePlayer(struct Player *player, struct Dungeon *dungeon)
-{
-    struct Room *currentRoom = &dungeon->rooms[player->currentRoom];
-
-    printf("\nIn front of you, you see %d room(s): ", currentRoom->ConnectedRoomsCount);
-    for (int i = 0; i < currentRoom->ConnectedRoomsCount; i++)
-    {
-        printf("Room %d", currentRoom->connectedRooms[i]);
-        if (i < currentRoom->ConnectedRoomsCount - 1)
-        {
-            printf(", ");
-        }
-    }
-    printf(".\n");
-
-    char input[10];
-    while (1)
-    {
-        printf("Which one will you choose? ");
-        scanf("%s", input);
-
-        // Convert input to an integer for room selection
-        int chosenRoom = atoi(input);
-
-        // Check if the chosen room is valid
-        int isValid = 0;
-        for (int i = 0; i < currentRoom->ConnectedRoomsCount; i++)
-        {
-            if (currentRoom->connectedRooms[i] == chosenRoom)
-            {
-                isValid = 1;
-                break;
-            }
-        }
-
-        if (isValid)
-        {
-            player->currentRoom = chosenRoom;
-            printf("\nYou have moved to Room %d.\n", player->currentRoom);
-            break;
-        }
-        else
-        {
-            printf("You cannot go to Room %d from here. Please choose a valid room.\n", chosenRoom);
-        }
-    }
 }
 
 void Combat(struct Player *player, struct Room *room)
