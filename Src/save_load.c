83eaba5f9926b4587f456a6c61bd69ec81ff8008
Anish <145456302+Anish-Pun@users.noreply.github.com>
2025-05-22 16:26:09 +0200

    Added Room ID & connectedRooms in save function

--- Src/save_load.c
+++ Src/save_load.c
@@ -18,7 +18,24 @@ void SaveGame(struct Player* player, struct Dungeon* dungeon, const char* filena
     cJSON_AddNumberToObject(playerData, "hp", player->hp);
     cJSON_AddNumberToObject(playerData, "damage", player->damage);
     cJSON_AddItemToObject(root, "player", playerData);
-    
+
+    // Save dungeon data
+    cJSON* dungeonData = cJSON_CreateObject();
+    cJSON_AddNumberToObject(dungeonData, "roomCount", dungeon->roomCount);
+
+    cJSON* roomsArray = cJSON_CreateArray();
+    for (int i = 0; i < dungeon->roomCount; i++) {
+        struct Room* room = &dungeon->rooms[i];
+        cJSON* roomData = cJSON_CreateObject();
+        cJSON_AddNumberToObject(roomData, "id", room->id);
+        cJSON_AddItemToObject(roomData, "connectedRooms",
+        cJSON_CreateIntArray(room->connectedRooms, room->ConnectedRoomsCount));
+
+        cJSON_AddItemToArray(roomsArray, roomData);
+    }
+    cJSON_AddItemToObject(dungeonData, "rooms", roomsArray);
+    cJSON_AddItemToObject(root, "dungeon", dungeonData);
+
     // Write JSON to file
     char* jsonString = cJSON_Print(root);
     FILE* file = fopen(filename, "w");
@@ -37,5 +54,3 @@ void SaveGame(struct Player* player, struct Dungeon* dungeon, const char* filena
     cJSON_Delete(root);
     free(jsonString);
 }
-
-
